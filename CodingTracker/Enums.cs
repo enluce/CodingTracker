e06c1e28aeda2acb9d369062d46ba32976ff6e54
enluce <86408122+enluce@users.noreply.github.com>
2025-04-10 00:11:32 +0530

    Add project files.

---
+++ CodingTracker/Enums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker
+{
+    internal class Enums
+    {
+        internal enum MenuOption
+        {
+            View,
+            Add,
+            Update,
+            Delete
+        }
+    }
+}
---
+++ CodingTracker/Program.cs
@@ -0,0 +1,34 @@
+﻿using Spectre.Console;
+using System.Configuration;
+using System.Collections.Specialized;
+using static CodingTracker.Enums;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+
+        AnsiConsole.Markup("Hello and welcome to the Coding Tracker!\n\n");
+
+        MenuOption menuSelection = AnsiConsole.Prompt(new SelectionPrompt<MenuOption>()
+            .Title("[green]Select[/] what you want to do.")
+            .AddChoices(Enum.GetValues<MenuOption>()));
+
+
+        switch (menuSelection)
+        {
+            case MenuOption.View:
+                ViewSessions();
+                break;
+            case MenuOption.Add:
+                AddSession();
+                break;
+            case MenuOption.Delete:
+                DeleteSession();
+                break;
+            case MenuOption.Update:
+                UpdateSession();
+                break;
+        }
+    }
+}
